c467ce8d15b963e917d33671e09d0ba454075ca9
promavto <promavto@ntmp.ru>
2024-03-03 12:15:28 +0300

    ff

--- net_io.c
+++ net_io.c
@@ -54,7 +54,8 @@
 
 #include <assert.h>
 #include <stdarg.h>
-#include <termios.h>
+#include <fcntl.h>  // for open()
+#include <unistd.h> // for write()
 //
 // ============================= Networking =============================
 //
@@ -513,45 +514,8 @@ static void modesSendRawOutput(struct modesMessage *mm, struct aircraft *a) {
   // Don't ever forward mlat messages via raw output.
     // int serial_port = open("/dev/serial0", O_RDWR);
 	//int serial_port = open("/dev/ttyAMA0", O_RDWR);
-    int serial_port = open("/dev/ttyS0", O_RDWR); // OrangePi
-    struct termios tty;
-
-    if (tcgetattr(serial_port, &tty) != 0)
-    {
-        printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
-    }
-    /* настройки порта */
-    tty.c_cflag &= ~PARENB;
-    tty.c_cflag &= ~CSTOPB;
-    tty.c_cflag &= ~CSIZE;
-    tty.c_cflag |= CS8;
-    tty.c_cflag &= ~CRTSCTS;
-    tty.c_cflag |= CREAD | CLOCAL;
-
-    tty.c_lflag &= ~ICANON;
-    tty.c_lflag &= ~ECHO;
-    tty.c_lflag &= ~ECHOE;
-    tty.c_lflag &= ~ECHONL;
-    tty.c_lflag &= ~ISIG;
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL);
-
-    tty.c_oflag &= ~OPOST;
-    tty.c_oflag &= ~ONLCR;
-
-    tty.c_cc[VTIME] = 10;
-    tty.c_cc[VMIN] = 0;
-
-    cfsetispeed(&tty, B115200);
-    cfsetospeed(&tty, B115200);
-
-    if (tcsetattr(serial_port, TCSANOW, &tty) != 0)
-    {
-        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
-    }
-	//printf("Test RTLSDR ttyAMA0\n");
-
-
+    // Open the serial port as a file
+    int serial_port = open("/dev/ttyS0", O_WRONLY);
 
  if (mm->source == SOURCE_MLAT)
         return;
@@ -589,10 +553,12 @@ static void modesSendRawOutput(struct modesMessage *mm, struct aircraft *a) {
 
     // write(serial_port, msg, msgLen);
 	// write(serial_port, msg, p - (char*)msg);
+	 write(serial_port, p, strlen(p));
+     close(serial_port);
 	 
     completeWrite(&Modes.raw_out, p);
-	 write(serial_port, msg, msgLen);
-     close(serial_port);
+	// write(serial_port, *p, strlen(p));
+    // close(serial_port);
 }
 
 static void send_raw_heartbeat(struct net_service *service)
