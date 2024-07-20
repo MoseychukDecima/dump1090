c1fd439f814a7a64c8b1d711fd415c8b64019fda
promavto <promavto@ntmp.ru>
2024-07-20 07:14:41 +0300

    ff

--- dump1090.h
+++ dump1090.h
@@ -200,7 +200,7 @@ struct ToDUMP1090
 {
     uint32_t  addr;           // ICAO address
 	char      squawk[5];               // Squawk
-    char      flight[9];      // Flight number	
+    char      flight[16];     // Flight number	
     int       altitude;       // Altitude
     int       speed;          // Velocity
     int       track;          // Angle of flight
