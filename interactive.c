66e7974fec2126b3eb5b2d98bfbb5482dabfb862
promavto <promavto@ntmp.ru>
2024-08-11 08:52:28 +0300

    ff

--- interactive.c
+++ interactive.c
@@ -444,7 +444,7 @@ void interactiveShowData(void) {
     }
     printf(
 "-------------------------------------------------------------------------------\n");
-
+   struct ToDUMP1090 sendBuf;
     while(a && (count < Modes.interactive_rows)) 
 	{
 
@@ -521,6 +521,7 @@ void interactiveShowData(void) {
                     strLat, strLon, signalAverage, msgs, (int)(now - a->seen));
 					
 					//int serial_port = open("/dev/ttyAMA0", O_RDWR);
+					/*
                     int serial_port = open("/dev/ttyS0", O_RDWR); //  
                     struct termios tty;
 
@@ -561,8 +562,8 @@ void interactiveShowData(void) {
                     {
                        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
                     }
-
-					struct ToDUMP1090 sendBuf;
+*/
+					//struct ToDUMP1090 sendBuf;
 					memset(&sendBuf,0, sizeof(sendBuf)); // Очистить массив
 					
 					sendBuf.addr = a->addr;                                   // ICAO address
@@ -612,7 +613,47 @@ void interactiveShowData(void) {
         }
         a = a->next;
     }
-	
+	                int serial_port = open("/dev/ttyS0", O_RDWR); //  
+                    struct termios tty;
+
+                    if (tcgetattr(serial_port, &tty) != 0)
+                    {
+                       printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+                    }
+                    // настройки порта 
+                        tty.c_cflag &= ~PARENB;   // Clear parity bit, disabling parity (most common)
+                        tty.c_cflag &= ~CSTOPB;   // Clear stop field, only one stop bit used in communication (most common)
+                        tty.c_cflag &= ~CSIZE;    // Clear all the size bits, then use one of the statements bel
+                        tty.c_cflag |= CS8;       // 8 bits per byte (most common)
+                        tty.c_cflag &= ~CRTSCTS;  // Disable RTS/CTS hardware flow control (most common)
+                        tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+                        tty.c_lflag &= ~ICANON;   //Canonical mode is disabled with:
+                        tty.c_lflag &= ~ECHO;     // Disable echo
+                        tty.c_lflag &= ~ECHOE;    // Disable erasure
+                        tty.c_lflag &= ~ECHONL;   // Disable new-line echo
+                        tty.c_lflag &= ~ISIG;     // Disable interpretation of INTR, QUIT and SUSP
+                        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+                        tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL); // Disable any special handling of received bytes
+
+                        tty.c_oflag &= ~OPOST;   // Prevent special interpretation of output bytes (e.g. newline chars)
+                        tty.c_oflag &= ~ONLCR;   // Prevent conversion of newline to carriage return/line feed 
+
+                        tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+                        tty.c_cc[VMIN] = 0;
+
+                        cfsetispeed(&tty, B19200);
+                        cfsetospeed(&tty, B19200);
+						
+						// cfsetispeed(&tty, B115200);
+                        //cfsetospeed(&tty, B115200);
+						
+
+                    if (tcsetattr(serial_port, TCSANOW, &tty) != 0)
+                    {
+                       printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+                    }
+
 	
                     /* Проверяем наличие новой информации */
 	                if ((mstime() - Modes.last_update1090) > MODES_INTERACTIVE_1090_TIME)
